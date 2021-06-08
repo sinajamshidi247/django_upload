9e437960edcc7719d721379f8d63bc45eb65fe6f
sina <sina.j372@gmail.com>
2021-06-09 00:03:31 +0430

    login complited

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
+
 
 def home(request):
+    if request.method == 'POST':
+        data = request.POST
+        username = data["username"]
+        password = data["password"]
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            messages.success(request, "login successfully", "success")
+        else:
+            print("user not exist")
+            messages.error(request, 'wrong username or password', 'warning')
+            return redirect('accounts:home')
+    else:
+        None
 
-
-    return render(request,"accounts/home.html")
+    return render(request, "accounts/home.html")
